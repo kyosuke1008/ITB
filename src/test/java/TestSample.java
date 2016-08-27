61ce7120a9385f0889b034edd2b4666066e74d98
石津恭輔 <kyosuke.ishizu@f.flect.co.jp>
2016-08-28 01:09:24 +0900

    first commit

---
+++ src/test/java/TestSample.java
@@ -0,0 +1,36 @@
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import static org.junit.Assert.assertEquals;
+
+public class TestSample {
+
+
+    public static void setUpBeforeClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownAfterClass() throws Exception {
+    }
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void test() {
+        FirefoxDriver driver = new FirefoxDriver();
+        driver.get("https://www.hotpepper.jp/");
+        driver.findElement(By.linkText("北海道")).click();
+        assertEquals("person.walkがおかしいよ！", "person.walkがおかしいよ！");
+    }
+
+}
