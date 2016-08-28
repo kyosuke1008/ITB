8240f1bd788c194f40fba66758eef4eaa4289bff
石津恭輔 <kyosuke.ishizu@f.flect.co.jp>
2016-08-28 18:01:10 +0900

    no message

--- src/test/java/TestSample.java
+++ src/test/java/TestSample.java
@@ -4,7 +4,6 @@ import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 
 import static org.junit.Assert.assertEquals;
 
